b48df242ae4250542660fc740cd922bba53a7326
Danilo Lima <danilo.moura.lima@gmail.com>
2017-08-27 17:08:33 +0200

    Add weather service

---
+++ app/services/weather_report.rb
@@ -0,0 +1,134 @@
+require "httparty"
+
+class WeatherReport
+  include HTTParty
+
+  attr_accessor :options, :response
+
+  base_uri        'api.openweathermap.org/data/'
+  default_timeout 1
+
+  def initialize(options = {})
+    @options = options
+  end
+
+  def fetch
+    @response = request_service
+  end
+
+  private
+
+  def build_params
+    request_params = {
+      appid: Rails.application.secrets.weather_api_key,
+      units: UNITS[:celsius],
+    }
+
+    if options[:city].present?
+      request_params.merge(q: options[:city])
+    elsif options[:lat].present? && options[:lon].present?
+      request_params.merge(lat: options[:lat], lon: options[:lon])
+    else
+      request_params.merge(lat: rand_lat, lon: rand_lon)
+    end
+  end
+
+  def rand_lat
+    rand(-90.000000...90.000000)
+  end
+
+  def rand_lon
+    rand(-180.000000...180.000000)
+  end
+
+  def build_icon_link(icon)
+    "#{ICON_URL}/#{icon}.png"
+  end
+
+  def request_service
+    begin
+      handle_timeouts do
+        handle_caching do
+          self.class.get("/2.5/weather?", { query: build_params })
+        end
+      end
+    rescue
+      build_response({ "cod" => 500, "message" => "Internal Error"})
+    end
+  end
+
+  def handle_timeouts
+    begin
+      yield
+    rescue Net::OpenTimeout, Net::ReadTimeout
+      build_response({ "cod" => 408, "message" => "Request Timeout: execution expired" })
+    end
+  end
+
+  def cache_key
+    if build_params[:q].present?
+      "weather_app:city:#{ build_params[:q] }"
+    else
+      "weather_app:lat_x_lon:#{build_params[:lat]}_#{build_params[:lon]}"
+    end
+  end
+
+  def handle_caching
+    if cached = Rails.cache.fetch(cache_key)
+      build_response(JSON[cached])
+    else
+      yield.tap do |result|
+        if result.success?
+          Rails.cache.write(cache_key, result.to_json)
+        end
+
+        return build_response(JSON[result.to_json])
+      end
+    end
+  end
+
+  def build_response(api_response)
+    if api_response["cod"] == 200
+      build_success_response(api_response)
+    else
+      build_error_response(api_response)
+    end
+  end
+
+  def build_success_response(api_response)
+    OpenStruct.new(
+      code: api_response["cod"],
+      city: api_response["name"],
+      country: api_response["sys"]["country"],
+      lat: api_response["coord"]["lat"],
+      lon: api_response["coord"]["lon"],
+      weather: {
+        main: api_response["weather"][0]["main"],
+        description: api_response["weather"][0]["description"],
+        icon: build_icon_link(api_response["weather"][0]["icon"]),
+        temp: api_response["main"]["temp"],
+        min: api_response["main"]["temp_min"],
+        max: api_response["main"]["temp_max"],
+        humidity: api_response["main"]["humidity"],
+        pressure: api_response["main"]["pressure"]
+      },
+      success?: true
+    )
+  end
+
+  def build_error_response(api_response)
+    OpenStruct.new(
+      code: api_response["cod"].to_i,
+      message: api_response["message"],
+      success?: false
+    )
+  end
+
+  ICON_URL    = "http://openweathermap.org/img/w".freeze
+
+  UNITS = {
+    fahrenheit: "imperial",
+    celsius:    "metric",
+    kelvin:     "",
+  }.freeze
+end
---
+++ spec/services/weather_report_spec.rb
@@ -0,0 +1,87 @@
+require 'rails_helper'
+
+describe WeatherReport do
+  let(:weather_report) { described_class.new(options) }
+
+  context ".fetch", vcr: { record: :once } do
+    subject { weather_report.response }
+
+    context "by city" do
+      context "when the city exists" do
+        let(:options) { { city: "Berlin" } }
+
+        before { weather_report.fetch }
+
+        it "returns the city weather" do
+          expect(subject).to be_a OpenStruct
+          expect(subject.success?).to be true
+          expect(subject.code).to           eq 200
+          expect(subject.city).to           eq "Berlin"
+          expect(subject.weather[:main]).to eq "Clouds"
+        end
+      end
+
+      context "when the city doesn't exist" do
+        let(:options) { { city: "12312212121" } }
+
+        before { weather_report.fetch }
+
+        it "returns the city weather" do
+          expect(subject).to be_a OpenStruct
+          expect(subject.success?).to be false
+          expect(subject.code).to     eq 404
+          expect(subject.message).to  eq "city not found"
+        end
+      end
+    end
+
+    context "by geolocation" do
+      let(:options) { { lat: "-23.43", lon: -45.07 } }
+
+      before { weather_report.fetch }
+
+      it "returns the Berlin weather" do
+        expect(subject).to be_a OpenStruct
+        expect(subject.success?).to be true
+        expect(subject.code).to           eq 200
+        expect(subject.city).to           eq "Ubatuba"
+        expect(subject.weather[:main]).to eq "Clear"
+      end
+    end
+
+    context "by random location" do
+      let(:options) { { } }
+
+      before do
+        allow(weather_report).to receive(:rand_lat).and_return(27.33)
+        allow(weather_report).to receive(:rand_lon).and_return(98.09)
+        weather_report.fetch
+      end
+
+      it "returns Random weather" do
+        expect(subject).to be_a OpenStruct
+        expect(subject.success?).to be true
+        expect(subject.code).to           eq 200
+        expect(subject.city).to           eq "Cikai"
+        expect(subject.weather[:main]).to eq "Rain"
+      end
+    end
+
+    context "when request reaches timeout" do
+      let(:response) { OpenStruct.new(code: 408, message: "Request Timeout: execution expired", success?: false) }
+      let(:options) { { city: "Berlin" } }
+
+      before do
+        allow(weather_report).to receive(:handle_timeouts).and_return(response)
+        weather_report.fetch
+      end
+
+      it "returns Random weather" do
+        expect(subject).to be_a OpenStruct
+        expect(subject.success?).to be false
+        expect(subject.code).to     eq 408
+        expect(subject.message).to  eq "Request Timeout: execution expired"
+      end
+    end
+  end
+end
